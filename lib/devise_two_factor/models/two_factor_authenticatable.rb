d88660ea3ddab82f0d9cdc07cefa28359032359b
Marcin Urban <urban.marcin@hotmail.com>
2023-07-28 14:20:02 +0200

    Update two_factor_authenticatable.rb

--- lib/devise_two_factor/models/two_factor_authenticatable.rb
+++ lib/devise_two_factor/models/two_factor_authenticatable.rb
@@ -34,6 +34,10 @@ module Devise
       # This defaults to the model's otp_secret
       # If this hasn't been generated yet, pass a secret as an option
       def validate_and_consume_otp!(code, options = {})
+        @valid_otp = validate_and_consume_otp_code!(code, options)
+      end
+
+      def validate_and_consume_otp_code!(code, options = {})
         otp_secret = options[:otp_secret] || self.otp_secret
         return false unless code.present? && otp_secret.present?
 
@@ -51,6 +55,14 @@ module Devise
         false
       end
 
+      def unauthenticated_message
+        if !@valid_otp
+          :invalid_otp
+        else
+          super
+        end
+      end
+      
       def otp(otp_secret = self.otp_secret)
         ROTP::TOTP.new(otp_secret)
       end
